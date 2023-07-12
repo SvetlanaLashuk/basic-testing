359208adf701177de95ccc3141dc0a8830c13bfa
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-07-12 09:11:03 +0300

    feat: add 01-simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,71 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 5,
+      b: 6,
+      action: Action.Add
+    };
+    const result = simpleCalculator(input);
+    expect(result).toBe(11);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 7,
+      b: 4,
+      action: Action.Subtract
+    };
+    const result = simpleCalculator(input);
+    expect(result).toBe(3);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 9,
+      b: 3,
+      action: Action.Multiply
+    };
+    const result = simpleCalculator(input);
+    expect(result).toBe(27);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 8,
+      b: 4,
+      action: Action.Divide
+    }
+    const result = simpleCalculator(input);
+    expect(result).toBe(2);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const input = {
+      a: 3,
+      b: 3,
+      action: Action.Exponentiate
+    };
+    const result = simpleCalculator(input);
+    expect(result).toBe(27);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const input = { 
+      a: 5, 
+      b: 2, 
+      action: '' 
+    };
+    const result = simpleCalculator(input);
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 4, b: 'string', action: Action.Add })).toBeNull();
+    expect(simpleCalculator({ a: false, b: 1, action: Action.Subtract })).toBeNull();
+    expect(simpleCalculator({ a: 5, b: null, action: Action.Multiply })).toBeNull();
+    expect(simpleCalculator({ a: 6, b: [1, 2], action: Action.Divide })).toBeNull();
+    expect(simpleCalculator({ a: {a: 4, b: 3}, b: 2, action: Action.Exponentiate })).toBeNull();
   });
 });
